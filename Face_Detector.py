f1e3f962098c69a5400ea47c94c9081c0e5b3e44
KevinJohnv <varghese.kevin.john@gmail.com>
2023-07-30 23:56:03 -0400

    Finished, was able to get the program to idetify faces based off of input images

--- Face_Detector.py
+++ Face_Detector.py
@@ -2,6 +2,22 @@ import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# Image being used to detect face
 img = cv2.imread('MR.jpg')
+
+# Gray scale the image
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+
+# Draw rectangle around the face
+(x,y, w, h) = face_coordinates[0]
+cv2.rectangle((img),(x,y), (x+w, y+h), (0,255,0), 2)
+
+cv2.imshow("Kevin's Face Detector",img)
+cv2.waitKey()
+
 
 print("Code Completed")
